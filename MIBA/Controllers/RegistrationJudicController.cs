52635207a8e94031837e0bdf73e838aa0619fe29
Maxim Shamrev <shmarev082@gmail.com>
2023-02-18 23:14:04 +0300

    registration judic except of create page

--- MIBA/Controllers/RegistrationJudicController.cs
+++ MIBA/Controllers/RegistrationJudicController.cs
@@ -1,4 +1,5 @@
 ﻿using MIBA.Data;
+using MIBA.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,6 +19,80 @@ namespace MIBA.Controllers
             var regs = _db.RegistrationJudic.Include(x=>x.Studies).ToList();
 
             return View(regs);
+        }
+
+        public IActionResult Create()
+        {
+            //var reg = new RegistrationJudic();
+
+            //reg.Studies = _db.Studies.ToList();
+
+            //return View(reg);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Create(RegistrationJudic registration)
+        {
+            _db.RegistrationJudic.Add(registration);
+            _db.SaveChanges();
+
+            TempData["Success"] = "Заявка была успешно создана";
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+
+            var entity = _db.RegistrationJudic.Include(x=>x.Studies).First(x=>x.Id == id);
+
+            if (entity == null)
+                return NotFound();
+
+            return View(entity);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(RegistrationJudic registration)
+        {
+            if (!ModelState.IsValid)
+                return View(registration);
+
+
+            var entity = _db.RegistrationJudic.Find(registration.Id);
+
+            if (entity == null)
+                return NotFound();
+
+            _db.RegistrationJudic.Update(registration);
+            _db.SaveChanges();
+
+            TempData["Success"] = "Заявка была успешно изменена";
+
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var entity = _db.RegistrationJudic.Find(id);
+
+            if (entity == null)
+                return NotFound();
+
+            _db.RegistrationJudic.Remove(entity);
+            _db.SaveChanges();
+
+            TempData["success"] = "Заявка успешно удалена";
+
+            return RedirectToAction("Index");
         }
     }
 }
