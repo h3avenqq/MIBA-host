d2156239e4b586b2c7d810d20c05e4ac584df554
Maxim Shamrev <shmarev082@gmail.com>
2023-02-18 18:03:55 +0300

    added create, edit and delete for feedbacks in admin panel

--- MIBA/Controllers/FeedbackController.cs
+++ MIBA/Controllers/FeedbackController.cs
@@ -1,4 +1,5 @@
 ﻿using MIBA.Data;
+using MIBA.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MIBA.Controllers
@@ -17,6 +18,55 @@ namespace MIBA.Controllers
             var feedbacks = _db.Feedbacks.ToList();
 
             return View(feedbacks);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Feedback feedback)
+        {
+            _db.Feedbacks.Add(feedback);
+            _db.SaveChanges();
+
+            TempData["success"] = "Отзыв успешно создан";
+
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            var feedback = _db.Feedbacks.FirstOrDefault(x => x.Id == id);
+
+            return View(feedback);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Feedback feedback)
+        {
+            _db.Feedbacks.Update(feedback);
+            _db.SaveChanges();
+
+            TempData["success"] = "Отзыв успешно изменен";
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            var obj = _db.Feedbacks.Find(id);
+
+            if (obj == null)
+                return NotFound();
+
+            _db.Feedbacks.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Отзыв успешно удален";
+
+            return RedirectToAction("Index");
         }
     }
 }
